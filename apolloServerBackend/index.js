8484d5e9e128ca0d84d3e431ef3e43ad9ca1b622
avinash_parkale <avinashparkale@gmail.com>
2022-12-15 23:12:31 +0530

    final commit

--- apolloServerBackend/index.js
+++ apolloServerBackend/index.js
@@ -2,7 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import axios from 'axios';
 let data;
-let singleData 
+let singleData
 
 const typeDefs = `#graphql
   type Repo {
@@ -26,52 +26,54 @@ const typeDefs = `#graphql
 
 const resolvers = {
     Query: {
-        Repos:()=> data,
-        RepoDetails:async (_, args)=>{
+        Repos: () => data,
+        RepoDetails: async (_, args) => {
             singleData = ''
             let name = args.name;
-            data.forEach(element => { 
-                if(element.name === name){
-                     singleData =  element;
-                }     
+            data.forEach(element => {
+                if (element.name === name) {
+                    singleData = element;
+                }
             });
-            if(singleData){
-            let url = `https://api.github.com/repos/avinashparkale/${name}/contents/`
-            let repoFilesArr = await axios.get(url);
-            singleData.NoOfFilesInRepo = repoFilesArr.data.length;
-            let fileName = 'README.md';
-            let contentUrl = `https://api.github.com/repos/avinashparkale/repoB/contents/New%20folder%20-%20Copy%20(2)/${fileName}?ref=master`
-            let fileContent = await axios.get(contentUrl);
-            console.log(fileContent.data.content)
-            const buff = Buffer.from(fileContent.data.content, 'base64');
-            const str = buff.toString('utf-8');
-            console.log(str);
-            singleData.FileContent = str;
-            return singleData
+            if (singleData) {
+                let url = `https://api.github.com/repos/avinashparkale/${name}/contents/`
+                let repoFilesArr = await axios.get(url);
+                singleData.NoOfFilesInRepo = repoFilesArr.data.length;
+                let fileName = 'README.md';
+                let contentUrl = `https://api.github.com/repos/avinashparkale/repoB/contents/New%20folder%20-%20Copy%20(2)/${fileName}?ref=master`
+                let fileContent = await axios.get(contentUrl);
+                console.log(fileContent.data.content)
+                const buff = Buffer.from(fileContent.data.content, 'base64');
+                const str = buff.toString('utf-8');
+                console.log(str);
+                singleData.FileContent = str;
+                return singleData
             }
         },
-    },    
+    },
 };
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
-getUserRepos().then(async ()=>{const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+getUserRepos().then(async () => {
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`Server ready at: ${url}`);
 });
-console.log(`Server ready at: ${url}`);});
 
 
 async function getUserRepos() {
-	try {
-		const response = await axios.get("https://api.github.com/users/avinashparkale/repos",{
-            OAUth : process.env.GITHUB_ACCESS_TOKEN
+    try {
+        const response = await axios.get("https://api.github.com/users/avinashparkale/repos", {
+            OAUth: process.env.GITHUB_ACCESS_TOKEN
         });
         data = response.data;
         return data;
-	}
-	catch (error) {
-		console.log(error);
-	}
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
