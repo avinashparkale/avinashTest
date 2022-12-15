5343bae161c32882a852a37142eefde0d31e2e4a
avinash_parkale <avinashparkale@gmail.com>
2022-12-15 23:09:55 +0530

    final commit

--- apolloServerBackend/index.js
+++ apolloServerBackend/index.js
@@ -2,6 +2,8 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import axios from 'axios';
 let data;
+let singleData 
+
 const typeDefs = `#graphql
   type Repo {
     name: String
@@ -11,22 +13,45 @@ const typeDefs = `#graphql
   type RepoDetail {
     name: String
     full_name : String
-    size: String
+    size: Int
     private: Boolean
+    NoOfFilesInRepo: Int
+    fileContent: String
   }
   type Query {
     Repos: [Repo]
-    RepoDetails:[RepoDetail]
+    RepoDetails(name: String):RepoDetail
   }
 `;
 
 const resolvers = {
     Query: {
         Repos:()=> data,
-        RepoDetails:()=> data,
+        RepoDetails:async (_, args)=>{
+            singleData = ''
+            let name = args.name;
+            data.forEach(element => { 
+                if(element.name === name){
+                     singleData =  element;
+                }     
+            });
+            if(singleData){
+            let url = `https://api.github.com/repos/avinashparkale/${name}/contents/`
+            let repoFilesArr = await axios.get(url);
+            singleData.NoOfFilesInRepo = repoFilesArr.data.length;
+            let fileName = 'README.md';
+            let contentUrl = `https://api.github.com/repos/avinashparkale/repoB/contents/New%20folder%20-%20Copy%20(2)/${fileName}?ref=master`
+            let fileContent = await axios.get(contentUrl);
+            console.log(fileContent.data.content)
+            const buff = Buffer.from(fileContent.data.content, 'base64');
+            const str = buff.toString('utf-8');
+            console.log(str);
+            singleData.FileContent = str;
+            return singleData
+            }
+        },
     },    
 };
-
 
 const server = new ApolloServer({
     typeDefs,
